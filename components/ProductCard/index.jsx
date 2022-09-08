20950bcdadd437128d92271091a2874a73aeb794
Wahyu Budi <wahyubudies1@gmail.com>
2022-09-08 18:20:28 +0700

    Menyelesaikan Product List dan Product Card

--- components/ProductCard/index.jsx
+++ components/ProductCard/index.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import styles from "./styles.module.css"
+import Image from "next/image"
 
 const ProductCard = () => {
   return (
-    <div>ProductCard</div>
+    <div className={styles.container}>
+      <Image src="/img/pizza.png" alt="" width="500" height="500" />
+      <h1 className={styles.title}>FIORI DI ZUCCA</h1>
+      <span className={styles.price}>Rp. 20K</span>
+      <p className={styles.desc}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </p>
+    </div>
   )
 }
 
--- components/ProductList/index.jsx
+++ components/ProductList/index.jsx
@@ -7,11 +7,15 @@ const ProductList = () => {
     <div className={styles.container}>
         <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
         <p className={styles.desc}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut blandit arcu
+            " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut blandit arcu
             in pretium molestie. Interdum et malesuada fames acme. Lorem ipsum dolor
-            sit amet, consectetur adipiscing elit.
+            sit amet, consectetur adipiscing elit. "
         </p>
         <div className={styles.wrapper}>
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
             <ProductCard />
             <ProductCard />
             <ProductCard />
